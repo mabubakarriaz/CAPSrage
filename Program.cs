ede2b04e7b1522d43a9f527fc934990036611027
Abubakar Riaz <abubakar.riaz@outlook.com>
2022-10-24 12:22:24 +0500

    basic code added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.WriteLine("CAPS rage started.");
+
+string line = string.Empty;
+
+while (true)
+{
+    Console.Write("Write: ");
+    line = Console.ReadLine();
+
+    if (line.ToLower() == "exit")
+    {
+        break;
+    }
+
+    Console.Write("RAGE: ");
+    Console.WriteLine(line.ToUpper());
+}
+
+Console.WriteLine("CAPS rage ended.");
+Console.ReadKey();
